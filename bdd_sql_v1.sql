bb704e267786fe58f0030b403c47b67e1fc6b2e5
jarod25 <kohler.jarod.2004@gmail.com>
2022-02-09 17:15:42 +0100

    signup et login fonctionne wola
    Pensez a relancer le sql avant, j'ai fait des modifs

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -31,8 +31,8 @@ CREATE TABLE user(
     id_user INT AUTO_INCREMENT,
     username VARCHAR(50),
     password VARCHAR(255),
+    role VARCHAR(50),
     est_actif tinyint(1),
-    pseudo VARCHAR(50),
     email VARCHAR(50),
     PRIMARY KEY(id_user)
 );
@@ -69,10 +69,14 @@ CREATE TABLE panier(
     CONSTRAINT fk_panier_article FOREIGN KEY (article_id) REFERENCES Ski(id_ski)
 );
 
+INSERT INTO user (id_user, email, username, password, role, est_actif) VALUES (NULL, 'admin@admin.fr', 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1);
+INSERT INTO user  (id_user, email, username, password, role, est_actif) VALUES (NULL, 'client@client.fr', 'client', 'sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4', 'ROLE_client', 1);
+INSERT INTO user  (id_user, email, username, password, role, est_actif) VALUES (NULL, 'client2@client2.fr', 'client2', 'sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3', 'ROLE_client', 1);
 LOAD DATA LOCAL INFILE 'type_ski.csv' INTO TABLE type_ski FIELDS TERMINATED BY ',';
 LOAD DATA LOCAL INFILE 'etat.csv' INTO TABLE etat FIELDS TERMINATED BY ',';
 LOAD DATA LOCAL INFILE 'ski.csv' INTO TABLE ski FIELDS TERMINATED BY ',';
 
+SELECT * FROM user;
 SELECT * FROM type_ski;
 SELECT * FROM etat;
 SELECT * FROM ski;
