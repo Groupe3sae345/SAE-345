7b802316e13743e26aa1549c2ecda0102850be1c
Nicolas Meister <91722355+seidja@users.noreply.github.com>
2022-02-18 17:55:09 +0100

    création commande pas fini

--- controllers/client_commande.py
+++ controllers/client_commande.py
@@ -12,6 +12,35 @@ client_commande = Blueprint('client_commande', __name__,
 @client_commande.route('/client/commande/add', methods=['POST'])
 def client_commande_add():
     mycursor = get_db().cursor()
+    client_id = session['user_id']
+    sql = "SELECT * FROM panier WHERE user_id=%s"
+    mycursor.execute(sql, client_id)
+    items_panier = mycursor.fetchall()
+    if items_panier is None or len(items_panier) < 1:
+        flash(u"Pas d'articles dans le panier")
+        return redirect(url_for('client_index'))
+
+    date_commande = datetime.now().strftime('XY-%n-%d XH:XM:%S')
+    tuple_insert = (date_commande, client_id, '1')
+    sql = "INSERT INTO commande(date_achat,user_id,etat_id) VALUES (%s,%s,%s)"
+    mycursor.execute(sql, tuple_insert)
+    sql = "SELECT Last_insert_id() as Last_insert_id"
+    mycursor.execute(sql)
+    commande_id = mycursor.fetchone()
+    print(commande_id, tuple_insert)
+
+    for item in items_panier:
+        tuple_insert = (client_id, item['stylo_id'])
+        sql = "DELETE FROM panier WHERE user_id = %s AND article_id = %s"
+        mycursor.execute(sql, tuple_insert)
+        sql = "SELECT prix_ski FROM ski WHERE id_ski = %s"
+        mycursor.execute(sql, item['stylo_id'])
+        prix = mycursor.fetchone()
+        sqt = "INSERT INTO Ligne_commande(commande_id,stylo_id, prix, quantite) VALUES (%s,%s,%s,%s)"
+        tuple_insert = (commande_id['last_insert_id'], item['stylo_id'], prix['prix'], item['quantite’])
+                        print(tuple_insert)
+        mycursor.execute(sql, tuple_insert)
+        get_db().commit()
     flash(u'Commande ajoutée')
     return redirect('/client/article/show')
     #return redirect(url_for('client_index'))
