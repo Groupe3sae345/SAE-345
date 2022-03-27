20ce0ae214649103a58d297dfd0cd45785aa8ce2
Nicolas Meister <91722355+seidja@users.noreply.github.com>
2022-03-27 21:06:30 +0200

    update

--- SAE_4/controllers/client_commentaire.py
+++ SAE_4/controllers/client_commentaire.py
@@ -8,9 +8,21 @@ from connexion_db import get_db
 client_commentaire = Blueprint('client_commentaire', __name__,
                         template_folder='templates')
 
+@client_commentaire.route('/client/comment/add', methods=['GET'])
+def add_commentaire():
+    mycursor = get_db().cursor()
+    sql='''SELECT * FROM avis'''
+    mycursor.execute(sql)
+    region = mycursor.fetchall()
+    return render_template('client/article/add_adresse.html', region=region)
+
 @client_commentaire.route('/client/comment/add', methods=['POST'])
 def client_comment_add():
     mycursor = get_db().cursor()
+    id = session["user_id"]
+    commentaire = request.form.get('commentaire', '')
+    note = request.form.get('note', '')
+    tuple_insert = (commentaire, note, id)
     article_id = request.form.get('idArticle', None)
 
     return redirect('/client/article/details/'+article_id)
